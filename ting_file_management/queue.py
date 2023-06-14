59e7e7e9e2b52126a4c06b86e1ea95986dd4b953
jonnoliveira <jonnutri@gmail.com>
2023-06-14 12:36:47 -0300

    Add: Queue class implementation - req01

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,16 +3,28 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self._data = list()
 
+    # verifica o tamanho da fila
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self._data)
 
+    # verifica se a fila está vazia
+    def is_empty(self):
+        return not bool(self.__len__())
+
+    # adiciona um elemento ao final da fila
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self._data.append(value)
 
+    # remove o primeiro elemento da fila
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if self.is_empty():
+            raise IndexError("The queue is empty")
+        return self._data.pop(0)
 
+    # busca um elemento na fila
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if self.is_empty() or index < 0 or index >= self.__len__():
+            raise IndexError(" Índice Inválido ou Inexistente")
+        return self._data[index]
