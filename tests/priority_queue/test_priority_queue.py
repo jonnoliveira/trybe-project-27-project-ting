34ddb7240bb5a802325952aef5786bba2f25025c
jonnoliveira <jonnutri@gmail.com>
2023-06-14 16:19:00 -0300

    Add: test for class PriorityQueue req06

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,32 @@
-# from ting_file_management.priority_queue import PriorityQueue
+import pytest
+from ting_file_management.priority_queue import PriorityQueue
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    test = PriorityQueue()
+
+    assert test.__len__() == 0
+
+    test.enqueue({"qtd_linhas": 1})
+    test.enqueue({"qtd_linhas": 2})
+    test.enqueue({"qtd_linhas": 3})
+    test.enqueue({"qtd_linhas": 4})
+
+    test.enqueue({"qtd_linhas": 5})
+    test.enqueue({"qtd_linhas": 61})
+    test.enqueue({"qtd_linhas": 71})
+    test.enqueue({"qtd_linhas": 91})
+
+    assert test.__len__() == 8
+    assert len(test.high_priority) == 4
+    assert len(test.regular_priority) == 4
+
+    test.dequeue()
+    test.dequeue()
+
+    assert test.__len__() == 6
+    assert test.search(0) == {"qtd_linhas": 3}
+    assert test.search(5) == {"qtd_linhas": 91}
+
+    with pytest.raises(IndexError):
+        test.search(777)
