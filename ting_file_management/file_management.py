f92210a7f6ecb3dc254b00da41bab16da8baef18
jonnoliveira <jonnutri@gmail.com>
2023-06-14 13:57:59 -0300

    Add: file_management implementation req02

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,21 @@
+import sys
+import os
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    # verifica existência do arquivo (True/False)
+    if not os.path.exists(path_file):
+        error = f"Arquivo {path_file} não encontrado"
+        print(error, file=sys.stderr)
+        return None
+
+    # verifica extensão do arquivo
+    file_extension = os.path.splitext(path_file)[1]
+    if file_extension != ".txt":
+        error = "Formato inválido"
+        print(error, file=sys.stderr)
+        return None
+
+    # lê o arquivo e retorna uma lista separada por "\n"
+    with open(path_file, "r") as arquivo:
+        return arquivo.read().split("\n")
